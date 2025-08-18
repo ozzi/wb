eb1cb8b354023ce01639c440ab7b56b0bf7cee6a
azhukov <alexander.zhukov@magenta-technology.com>
2025-08-18 18:51:21 +0400

    pid-controller: add tolerance, remove dynamic functional

--- rules/pid-controller.js
+++ rules/pid-controller.js
@@ -1,82 +1,37 @@
 //"use strict";
-var PID = function(Input, Setpoint, Kp, Ki, Kd, ControllerDirection, InitialOutput) {
+var PID = function (Input, Setpoint, Kp, Ki, Kd, ControllerDirection, InitialOutput) {
     this.input = Input;
     this.mySetpoint = Setpoint;
     this.inAuto = false;
     this.setOutputLimits(0, 100);
-    this.SampleTime = 1000;
+    this.sampleTime = 1000;
     this.setTunings(Kp, Ki, Kd);
     this.setControllerDirection(ControllerDirection);
-    this.lastTime = this.millis() - this.SampleTime;
+    this.lastTime = this.millis() - this.sampleTime;
     this.setIntegral(InitialOutput);
     this.setOutput(InitialOutput);
+    this.tolerance = 0;
     this.enableLogging = false;
-    this.alphaDynamicKp = 0;
-    this.maxDynamicKp = Kp;
 };
 
-PID.prototype.setInput = function(current_value) {
-    this.input = current_value;
+PID.prototype.setInput = function (NewInput) {
+    this.input = NewInput;
 };
 
-PID.prototype.setPoint = function(current_value) {
-    this.mySetpoint = current_value;
+PID.prototype.setPoint = function (SetPoint) {
+    this.mySetpoint = SetPoint;
 };
 
-PID.prototype.setLogging = function(new_value) {
-    this.enableLogging = new_value;
+PID.prototype.setLogging = function (EnableLogging) {
+    this.enableLogging = EnableLogging;
 }
 
-PID.prototype.setAlphaDynamicKp = function(new_value) {
-    this.alphaDynamicKp = new_value;
-}
-
-PID.prototype.setMaxDynamicKp = function(new_value) {
-    this.maxDynamicKp = new_value;
-}
-
-PID.prototype.millis = function() {
+PID.prototype.millis = function () {
     var d = new Date();
     return d.getTime();
 };
 
-PID.prototype.compute = function() {
-    if (!this.inAuto) {
-        return false;
-    }
-    var now = this.millis();
-    var timeChange = (now - this.lastTime);
-    if (timeChange > 0) {
-        var error = (this.mySetpoint - this.input) * this.setDirection;
-        var timeChangeInSec = timeChange / 1000;
-        var output = this.update(error, timeChangeInSec);
-        this.setOutput(output);
-        this.lastTime = now;
-        return true;
-    } else {
-        return false;
-    }
-};
-
-PID.prototype.getDynamicKp = function(error) {
-  return this.kp + (this.maxDynamicKp - this.kp) * (1 - Math.exp(-this.alphaDynamicKp * Math.abs(error)));
-}
-
-PID.prototype.update = function(error, time) {
-    var currentKp = this.getDynamicKp(error);
-    var proportional = currentKp * error;
-    var integral = this.integral + this.ki * time * (error + this.previousError) / 2;
-    var derivative = this.kd / time * (error - this.previousError);
-    var output = proportional + integral + derivative;
-    this.setIntegral(integral);
-    this.previousError = error;
-    if (this.enableLogging) {
-      log("pid E " + error + "; dP " + currentKp + " ; P " + proportional + "; I " + this.integral + "; D " + derivative + "; O " + output);
-    }
-    return output;
-}
-
-PID.prototype.setTunings = function(Kp, Ki, Kd) {
+PID.prototype.setTunings = function (Kp, Ki, Kd) {
     if (Kp < 0 || Ki < 0 || Kd < 0) {
         return;
     }
@@ -86,14 +41,14 @@ PID.prototype.setTunings = function(Kp, Ki, Kd) {
     this.kd = Kd;
 };
 
-PID.prototype.setSampleTime = function(NewSampleTime) {
+PID.prototype.setSampleTime = function (NewSampleTime) {
     if (NewSampleTime > 0) {
-        this.SampleTime = NewSampleTime;
-        this.lastTime = this.millis() - this.SampleTime;
+        this.sampleTime = NewSampleTime;
+        this.lastTime = this.millis() - this.sampleTime;
     }
 };
 
-PID.prototype.setOutput = function(val) {
+PID.prototype.setOutput = function (val) {
     if (val > this.outMax) {
         val = this.outMax;
     } else if (val < this.outMin) {
@@ -102,20 +57,18 @@ PID.prototype.setOutput = function(val) {
     this.myOutput = val;
 };
 
-PID.prototype.setIntegral = function(val) {
+PID.prototype.setIntegral = function (val) {
     if (val > this.outMax) {
         val = this.outMax;
     } else if (val < this.outMin) {
         val = this.outMin;
+    } else if (isNaN(val)) {
+        val = (this.outMax + this.outMin) / 2;
     }
     this.integral = val;
 };
 
-PID.prototype.setLogging = function(val) {
-    this.enableLogging = val;
-}
-
-PID.prototype.setOutputLimits = function(Min, Max) {
+PID.prototype.setOutputLimits = function (Min, Max) {
     if (Min >= Max) {
         return;
     }
@@ -133,7 +86,7 @@ PID.prototype.setOutputLimits = function(Min, Max) {
     }
 };
 
-PID.prototype.setMode = function(Mode) {
+PID.prototype.setMode = function (Mode) {
     var newAuto;
     if (Mode.toString().toLowerCase() == 'automatic' || Mode.toString().toLowerCase() == 'auto') {
         newAuto = 1;
@@ -149,13 +102,7 @@ PID.prototype.setMode = function(Mode) {
     this.inAuto = newAuto;
 };
 
-PID.prototype.initialize = function() {
-    this.setIntegral(this.myOutput);
-    this.previousError = 0;
-    this.lastTime = this.millis() - this.SampleTime;
-};
-
-PID.prototype.setControllerDirection = function(ControllerDirection) {
+PID.prototype.setControllerDirection = function (ControllerDirection) {
     if (ControllerDirection.toString().toLowerCase() == 'direct') {
         this.setDirection = 1;
     }
@@ -167,208 +114,281 @@ PID.prototype.setControllerDirection = function(ControllerDirection) {
     }
 };
 
-PID.prototype.getNormalizedOutput = function(normalizedMin, normalizedMax) {
-    var output = this.getOutput();
-    var outLength = this.outMax - this.outMin;
-    var normalizedOutput = (normalizedMax - normalizedMin) * output / outLength  + normalizedMin;
-    return normalizedOutput;
+PID.prototype.setTolerance = function (Tolerance) {
+    this.tolerance = Tolerance;
 }
 
-PID.prototype.getKp = function() {
+PID.prototype.getOutMin = function () {
+    return this.outMin;
+};
+
+PID.prototype.getOutMax = function () {
+    return this.outMax;
+};
+
+PID.prototype.getKp = function () {
     return this.kp;
 };
 
-PID.prototype.getKd = function() {
+PID.prototype.getKd = function () {
     return this.kd;
 };
 
-PID.prototype.getKi = function() {
+PID.prototype.getKi = function () {
     return this.ki;
 };
 
-PID.prototype.getMode = function() {
+PID.prototype.getMode = function () {
     return this.inAuto ? "Auto" : "Manual";
 };
 
-PID.prototype.getDirection = function() {
+PID.prototype.getDirection = function () {
     return this.controllerDirection;
 };
 
-PID.prototype.getOutput = function() {
+PID.prototype.getOutput = function () {
     return this.myOutput;
 };
 
-PID.prototype.getInput = function() {
+PID.prototype.getInput = function () {
     return this.input;
 };
 
-PID.prototype.getSetPoint = function() {
+PID.prototype.getSetPoint = function () {
     return this.mySetpoint;
 };
 
-PID.prototype.getIntegral = function() {
+PID.prototype.getIntegral = function () {
     return this.integral;
 };
+
+PID.prototype.initialize = function () {
+    this.setIntegral(this.myOutput);
+    this.previousError = 0;
+    this.lastTime = this.millis() - this.sampleTime;
+};
+
+PID.prototype.compute = function () {
+    if (!this.inAuto) {
+        return false;
+    }
+    var now = this.millis();
+    var timeChange = (now - this.lastTime);
+    if (timeChange > 0) {
+        var error = (this.mySetpoint - this.input) * this.setDirection;
+        if (Math.abs(error) > this.tolerance) {
+            var timeChangeInSec = timeChange / 1000;
+            var output = this.update(error, timeChangeInSec);
+            this.setOutput(output);
+            this.lastTime = now;
+            return true;
+        } else {
+            this.lastTime = now;
+            return false;
+        }
+    } else {
+        return false;
+    }
+};
+
+PID.prototype.update = function (error, time) {
+    var proportional = this.kp * error;
+    var integral = this.integral + this.ki * time * (error + this.previousError) / 2;
+    var derivative = this.kd / time * (error - this.previousError);
+    var output = proportional + integral + derivative;
+    this.setIntegral(integral);
+    this.previousError = error;
+    if (this.enableLogging) {
+        log("pid E " + error + " ; P " + proportional + "; I " + this.integral + "; D " + derivative + "; O " + output);
+    }
+    return output;
+}
 
 module.exports = PID;
 
 function makePIDController(
-  deviceName,
-  getValueClosure,
-  outputTopicName,
-  minOutput,
-  maxOutput,
-  direction,
-  timeframe,
-  enableLog
+    deviceName,
+    getValueClosure,
+    outputTopicName,
+    minOutput,
+    maxOutput,
+    direction,
+    timeframe
 ) {
-  var deviceName = "pid-controller-" + deviceName;
+    var deviceName = "pid-controller-" + deviceName;
     defineVirtualDevice(deviceName, {
-      title: "PID Controller: " + deviceName,
-      cells: {
-        enabled: {
-            title: "Enabled",
-            type: "switch",
-            value: false,
-            order: 1
-        },
-        set_point: {
-            title: "Target value",
-            type: "range",
-            value: 35,
-            max: 100,
-            min: 0,
-            order: 2
-        },
-        cp: {
-            title: "Proportional gain",
-            type: "range",
-            value: 1,
-            max: 100,
-            min: 0,
-            order: 3
-        },
-        ci: {
-            title: "Integral gain / 10",
-            type: "range",
-            value: 1,
-            max: 100,
-            min: 0,
-            order: 4
-        },
-        cd: {
-            title: "Derivative gain",
-            type: "range",
-            value: 1,
-            max: 100,
-            min: 0,
-            order: 5
-        },
-        alpha_proportional: {
-            title: "Alpha for dynamic proportional / 10",
-            type: "range",
-            value: 0,
-            max: 100,
-            min: 0,
-            order: 6
-        },
-        max_proportional: {
-            title: "Max dynamic proportional",
-            type: "range",
-            value: 0,
-            max: 200,
-            min: 0,
-            order: 7
-        },
-        logging: {
-            title: "logging",
-            type: "switch",
-            value: false,
-            order: 10
-        },
-        power: {
-            title: "power",
-            type: "value",
-            value: 0,
-            read_only: true,
-            order: 20
-        },
-        integral: {
-            title: "integral",
-            type: "value",
-            value: 0,
-            read_only: true,
-            order: 21
+        title: "PID Controller: " + deviceName,
+        cells: {
+            enabled: {
+                title: "Enabled",
+                type: "switch",
+                value: false,
+                order: 1
+            },
+            set_point: {
+                title: "Target value",
+                type: "value",
+                value: 35,
+                readonly: false,
+                order: 2
+            },
+            cp: {
+                title: "Proportional gain",
+                type: "range",
+                value: 1,
+                max: 100,
+                min: 0,
+                order: 3
+            },
+            ci: {
+                title: "Integral gain / 10",
+                type: "range",
+                value: 1,
+                max: 100,
+                min: 0,
+                order: 4
+            },
+            cd: {
+                title: "Derivative gain",
+                type: "range",
+                value: 1,
+                max: 100,
+                min: 0,
+                order: 5
+            },
+            tolerance: {
+                title: "Tolerance",
+                type: "value",
+                value: 0,
+                readonly: false,
+                order: 6
+            },
+            logging: {
+                title: "logging",
+                type: "switch",
+                value: false,
+                order: 10
+            },
+            power: {
+                title: "power",
+                type: "value",
+                value: 0,
+                readonly: false,
+                order: 20
+            },
+            integral: {
+                title: "integral",
+                type: "value",
+                value: 0,
+                readonly: true,
+                order: 21
+            }
         }
-      }
     });
 
-  var enabledTopicName = deviceName + "/enabled";
-  var setPointTopicName = deviceName + "/set_point";
-  var cpTopicName = deviceName + "/cp";
-  var ciTopicName = deviceName + "/ci";
-  var cdTopicName = deviceName + "/cd";
-  var integralTopicName = deviceName + "/integral";
-  var powerTopicName = deviceName + "/power";
-  var loggingTopicName = deviceName + "/logging";
-  var alphaDynamicKpTopicName = deviceName + "/alpha_proportional";
-  var maxDynamicKpTopicName = deviceName + "/max_proportional";
-  var length = maxOutput - minOutput;
-  var normalizedPower = dev[outputTopicName] / length * 100;
-  
-  var ctr = new PID(
-    getValueClosure(),
-    dev[setPointTopicName],
-    dev[cpTopicName],
-    dev[ciTopicName] / 10,
-    dev[cdTopicName],
-    direction,
-    normalizedPower
-  );
-  ctr.setSampleTime(timeframe);
-  ctr.setMode("manual");
-  
-  var myControl = function() {
-    var enabled = dev[enabledTopicName];
-    if (enabled == true) {
-      ctr.setMode("automatic");
-    } else {
-      ctr.setMode("manual");
-    }
-	var temperature = getValueClosure(); 
-  	var temperatureSetpoint =  dev[setPointTopicName]; 
-    var Kp = dev[cpTopicName];
-    var Ki = dev[ciTopicName] / 10;
-    var Kd = dev[cdTopicName];
-    var enableLogging = dev[loggingTopicName];
-    var alphaDynamicKp = dev[alphaDynamicKpTopicName] / 10;
-    var maxDynamicKp = dev[maxDynamicKpTopicName];
-    ctr.setLogging(enableLogging);
-  	ctr.setInput(temperature);
-  	ctr.setPoint(temperatureSetpoint);
-  	ctr.setTunings(Kp, Ki, Kd);
-    ctr.setAlphaDynamicKp(alphaDynamicKp);
-    ctr.setMaxDynamicKp(maxDynamicKp);
-  	if (ctr.compute()) {
-        dev[powerTopicName] = ctr.getOutput();
-        dev[outputTopicName] = ctr.getNormalizedOutput(minOutput, maxOutput);
-        dev[integralTopicName] = ctr.getIntegral();
-    }
-  };
-  myControl();
-  setInterval(myControl, timeframe);
+    var enabledTopicName = deviceName + "/enabled";
+    var setPointTopicName = deviceName + "/set_point";
+    var cpTopicName = deviceName + "/cp";
+    var ciTopicName = deviceName + "/ci";
+    var cdTopicName = deviceName + "/cd";
+    var integralTopicName = deviceName + "/integral";
+    var powerTopicName = deviceName + "/power";
+    var loggingTopicName = deviceName + "/logging";
+    var toleranceTopicName = deviceName + "/tolerance";
+    var length = maxOutput - minOutput;
+    var normalizedPower = dev[outputTopicName] / length * 100;
+
+    var ctr = new PID(
+        getValueClosure(),
+        dev[setPointTopicName],
+        dev[cpTopicName],
+        dev[ciTopicName] / 10,
+        dev[cdTopicName],
+        direction,
+        normalizedPower
+    );
+    ctr.setSampleTime(timeframe);
+    ctr.setMode("manual");
+
+    var myControl = function () {
+        var enabled = dev[enabledTopicName];
+        if (enabled == true) {
+            ctr.setMode("automatic");
+        } else {
+            ctr.setMode("manual");
+        }
+        var temperature = getValueClosure();
+        var temperatureSetpoint = dev[setPointTopicName];
+        var Kp = dev[cpTopicName];
+        var Ki = dev[ciTopicName] / 10;
+        var Kd = dev[cdTopicName];
+        var tolerance = dev[toleranceTopicName];
+        var enableLogging = dev[loggingTopicName];
+        ctr.setLogging(enableLogging);
+        ctr.setInput(temperature);
+        ctr.setPoint(temperatureSetpoint);
+        ctr.setTunings(Kp, Ki, Kd);
+        ctr.setTolerance(tolerance);
+        if (ctr.compute()) {
+            dev[powerTopicName] = ctr.getOutput();
+            dev[integralTopicName] = ctr.getIntegral();
+        }
+    };
+    myControl();
+    setInterval(myControl, timeframe);
+
+    defineRule("pid-power-automation-" + deviceName, {
+        whenChanged: [
+          powerTopicName
+        ],
+        then: function (newValue) {
+            var outLength = ctr.getOutMax() - ctr.getOutMin();
+            var normalizedOutput = (maxOutput - minOutput) * newValue / outLength + minOutput;
+            dev[outputTopicName] = normalizedOutput;
+        }
+      });
 }
 
 makePIDController(
+    "dry-cooling",
+    function () {
+        return dev["wb-m1w2_225/External Sensor 2"];
+    },
+    "wb-mao4_100/Channel 1",
+    0,
+    10000,
+    "reverse",
+    900
+);
+
+makePIDController(
+    "air-asic",
+    function () {
+        return dev["wb-m1w2_225/External Sensor 1"];//wb-m1w2_69/External Sensor 2
+    },
+    "wb-mao4_100/Channel 2",
+    0,
+    10000,
+    "reverse",
+    900
+);
+
+makePIDController(
+    "esbe-actuator-asic",
+    function () {
+        return dev["wb-m1w2_33/External Sensor 1"];
+    },
+    "wb-mao4_100/Channel 3",
+    0,
+    10000,
+    "reverse",
+    900
+);
+
+makePIDController(
     "asic-pump",
-    function() {
-        var dryCoolingInput = dev["wb-m1w2_33/External Sensor 1"];
-        var dryCoolingOutput = dev["wb-m1w2_33/External Sensor 2"];
-        var value = dryCoolingOutput - dryCoolingInput;
-        if (value < 0) {
-          value = 0;
-        }
-        return value;
+    function () {
+        return dev["wb-m1w2_33/External Sensor 2"] - dev["wb-m1w2_33/External Sensor 1"];
     },
     "pump-pwm-controller-asic/power",
     0,
@@ -379,78 +399,24 @@ makePIDController(
 
 makePIDController(
     "dry-cooling-pump",
-    function() {
-        var dryCoolingInput = dev["wb-m1w2_225/External Sensor 1"];
-        var dryCoolingOutput = dev["wb-m1w2_225/External Sensor 2"];
-        var value = dryCoolingOutput - dryCoolingInput;
-        if (value < 0) {
-          value = 0;
-        }
-        return value;
+    function () {
+        return dev["politech-water-flow-sensor-dry-cooling/flow_lm"]; //dev["wb-m1w2_225/External Sensor 1"] - dev["wb-m1w2_225/External Sensor 2"];
     },
     "pump-pwm-controller-dry-cooling-tower/power",
     0,
     1000,
-    "reverse",
+    "direct",
     900
 );
 
 makePIDController(
-    "dry-cooling",
-    function() {
-        return dev["wb-m1w2_225/External Sensor 2"];
+    "pool-heat-exchanger-pump",
+    function () {
+        return dev["wb-m1w2_118/External Sensor 2"] - dev["wb-m1w2_118/External Sensor 1"];
     },
-    "wb-mao4_213/Channel 2",
+    "pump-pwm-controller-pool-heat-exchanger/power",
     0,
-    10000,
+    1000,
     "reverse",
     900
-);
-
-makePIDController(
-  "esbe-actuator-asic",
-  function() {
-        return dev["wb-m1w2_33/External Sensor 1"];
-  },
-  "wb-mao4_213/Channel 1",
-  0,
-  10000,
-  "direct",
-  900
-);
-
-makePIDController(
-  "esbe-actuator-floor-1",
-  function() {
-        return dev["wb-ms_176/External Sensor 2"];
-  },
-  "wb_mao4_20ma_16/channel_2_level",
-  0,
-  100,
-  "direct",
-  900
-);
-
-makePIDController(
-  "esbe-actuator-floor-2",
-  function() {
-        return dev["wb-w1/28-3c01e0764bc1"];
-  },
-  "wb_mao4_20ma_16/channel_1_level",
-  0,
-  100,
-  "direct",
-  900
-);
-
-makePIDController(
-  "air-asic",
-  function() {
-        return dev["wb-m1w2_69/External Sensor 2"];
-  },
-  "wb-mao4_213/Channel 3",
-  0,
-  10000,
-  "reverse",
-  900
 );
