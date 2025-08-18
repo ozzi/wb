3b33f442f0e7276939e00040db276a73d7565b86
azhukov <alexander.zhukov@magenta-technology.com>
2025-08-18 22:17:02 +0400

    vnish-device: implement mining control

--- rules/vnish-device.js
+++ rules/vnish-device.js
@@ -22,6 +22,52 @@ function performGET(
     });
 }
 
+function performPOST(
+  host,
+  endpoint,
+  apikey,
+  callback
+) {
+  var command = 'curl -X POST {}{} -H \'x-api-key: {}\''.format(
+    host,
+    endpoint,
+    apikey
+  );
+
+  runShellCommand(
+    command,
+    {
+      captureOutput: false,
+      exitCallback: function (exitCode) {
+        if (exitCode === 0) {
+          callback(true);
+        } else {
+          callback(false);
+        }
+      }
+    });
+}
+
+function postMiningPause(host, apikey, callback) {
+  performPOST(host, "/api/v1/mining/pause", apikey, callback);
+}
+
+function postMiningResume(host, apikey, callback) {
+  performPOST(host, "/api/v1/mining/resume", apikey, callback);
+}
+
+function postMiningStop(host, apikey, callback) {
+  performPOST(host, "/api/v1/mining/stop", apikey, callback);
+}
+
+function postMiningStart(host, apikey, callback) {
+  performPOST(host, "/api/v1/mining/start", apikey, callback);
+}
+
+function postMiningRestart(host, apikey, callback) {
+  performPOST(host, "/api/v1/mining/restart", apikey, callback);
+}
+
 function getSummary(host, callback) {
   performGET(
     host,
@@ -71,26 +117,58 @@ function buildVNISHDevice(
         value: false,
         order: 1
       },
+      apikey: {
+        title: "api key",
+        type: "text",
+        value: "",
+        readonly: false,
+        order: 2
+      },
       state: {
         title: "miner state",
         type: "text",
         value: "",
         readonly: true,
-        order: 2
+        order: 3
       },
       current_preset: {
         title: "current preset",
         type: "value",
         value: 0,
         readonly: true,
-        order: 3
+        order: 4
       },
       power: {
         title: "current power",
         type: "value",
         value: 0,
         readonly: true,
-        order: 4
+        order: 5
+      },
+      start_mining: {
+        title: "start mining",
+        type: "pushbutton",
+        order: 6
+      },
+      stop_mining: {
+        title: "stop mining",
+        type: "pushbutton",
+        order: 7
+      },
+      pause_mining: {
+        title: "pause mining",
+        type: "pushbutton",
+        order: 8
+      },
+      resume_mining: {
+        title: "resume mining",
+        type: "pushbutton",
+        order: 9
+      },
+      restart_mining: {
+        title: "restart mining",
+        type: "pushbutton",
+        order: 10
       }
     }
   });
@@ -99,6 +177,13 @@ function buildVNISHDevice(
   var stateTopicName = deviceName + "/state";
   var currentPresetTopicName = deviceName + "/current_preset";
   var powerTopicName = deviceName + "/power";
+  var apikeyTopicName = deviceName + "/apikey";
+
+  var startMiningTopicName = deviceName + "/start_mining";
+  var stopMiningTopicName = deviceName + "/stop_mining";
+  var pauseMiningTopicName = deviceName + "/pause_mining";
+  var resumeMiningTopicName = deviceName + "/resume_mining";
+  var restartMiningTopicName = deviceName + "/restart_mining";
 
   var intervalId = null;
 
@@ -136,6 +221,101 @@ function buildVNISHDevice(
           timeframe
         );
       }
+    }
+  });
+
+  defineRule("vnish-start-button-automation-" + deviceName, {
+    whenChanged: [
+      startMiningTopicName
+    ],
+    then: function () {
+      postMiningStart(
+        hostName,
+        dev[apikeyTopicName],
+        function(value) {
+          var message = '{}: mining start with {}'.format(
+            deviceName,
+            value
+          );
+          log(message);
+        }
+      );
+    }
+  });
+
+  defineRule("vnish-stop-button-automation-" + deviceName, {
+    whenChanged: [
+      stopMiningTopicName
+    ],
+    then: function () {
+      postMiningStop(
+        hostName,
+        dev[apikeyTopicName],
+        function(value) {
+          var message = '{}: mining stop with {}'.format(
+            deviceName,
+            value
+          );
+          log(message);
+        }
+      );
+    }
+  });
+
+  defineRule("vnish-restart-button-automation-" + deviceName, {
+    whenChanged: [
+      restartMiningTopicName
+    ],
+    then: function () {
+      postMiningRestart(
+        hostName,
+        dev[apikeyTopicName],
+        function(value) {
+          var message = '{}: mining restart with {}'.format(
+            deviceName,
+            value
+          );
+          log(message);
+        }
+      );
+    }
+  });
+
+  defineRule("vnish-resume-button-automation-" + deviceName, {
+    whenChanged: [
+      resumeMiningTopicName
+    ],
+    then: function () {
+      postMiningResume(
+        hostName,
+        dev[apikeyTopicName],
+        function(value) {
+          var message = '{}: mining resume with {}'.format(
+            deviceName,
+            value
+          );
+          log(message);
+        }
+      );
+    }
+  });
+
+  defineRule("vnish-pause-button-automation-" + deviceName, {
+    whenChanged: [
+      pauseMiningTopicName
+    ],
+    then: function () {
+      postMiningPause(
+        hostName,
+        dev[apikeyTopicName],
+        function(value) {
+          var message = '{}: mining pause with {}'.format(
+            deviceName,
+            value
+          );
+          log(message);
+        }
+      );
     }
   });
 }
