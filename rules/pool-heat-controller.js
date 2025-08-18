cfcc7d8d25629a741fe59c62939f39fe3714a022
azhukov <alexander.zhukov@magenta-technology.com>
2025-08-18 18:49:02 +0400

    pool-heat-controller: initial version

---
+++ rules/pool-heat-controller.js
@@ -0,0 +1,107 @@
+function makePoolHeatController(
+    name,
+    currentTemperatureTopicName,
+    poolFiltrationModeTopicName,
+    heatOnClosure,
+    heatOffClosure
+) {
+    var deviceName = "pool-heat-ctrl-" + name;
+    defineVirtualDevice(deviceName, {
+        title: "Pool Heat Controller - " + name,
+        cells: {
+            mode: {
+                title: "mode",
+                type: "value",
+                value: 0,
+                readonly: false,
+                enum: {
+                  0: {en: "Off", ru: "Выключено"},
+                  1: {en: "Heat", ru: "Нагрев"}
+                }
+            },
+            target: {
+                title: "target temperature",
+                type: "value",
+                value: 20,
+                readonly: false
+            },
+            current: {
+                title: "current temperature",
+                type: "value",
+                type: "value",
+                value: 20,
+                readonly: true
+            },
+            tolerance: {
+                title: "tolerance",
+                type: "value",
+                value: 0,
+                readonly: false
+            },
+            active: {
+                title: "active",
+                type: "switch",
+                value: false,
+                readonly: true
+            }
+        }
+    });
+
+    var modeTopicName = deviceName + "/mode";
+    var targetTopicName = deviceName + "/target";
+    var activeTopicName = deviceName + "/active";
+    var hysteresisTopicName = deviceName + "/tolerance";
+    var currentTopicName = deviceName + "/current";
+
+    defineRule("mode-changed-" + name, {
+        whenChanged: [
+            modeTopicName,
+            targetTopicName,
+            currentTemperatureTopicName,
+            poolFiltrationModeTopicName,
+            hysteresisTopicName
+        ],
+        then: function () {
+            var mode = dev[modeTopicName];
+            var poolFiltrationMode = dev[poolFiltrationModeTopicName];
+            var currentTemperature = dev[currentTemperatureTopicName];
+            if (mode == 1 && poolFiltrationMode == 1) {
+                var targetTemperature = dev[targetTopicName];
+                var hysteresis = dev[hysteresisTopicName];
+                if (currentTemperature < (targetTemperature - hysteresis)) {
+                    dev[activeTopicName] = true;
+                } else if (currentTemperature > (targetTemperature + hysteresis)) {
+                    dev[activeTopicName] = false;
+                }
+            } else {
+                dev[activeTopicName] = false;
+            }
+            dev[currentTopicName] = currentTemperature;
+        }
+    });
+
+    defineRule("active-changed-" + name, {
+        whenChanged: [
+            activeTopicName
+        ],
+        then: function (newValue) {
+            if (newValue == false) {
+                heatOffClosure();
+            } else {
+                heatOnClosure();
+            }
+        }
+    });
+}
+
+makePoolHeatController(
+    "outdoor",
+    "wb-m1w2_69/External Sensor 1",
+    "pool-filtration-ctrl-outdoor/mode",
+    function () {
+        dev["ASIC-cooling-controller-T21/mode"] = 4;
+    },
+    function () {
+        dev["ASIC-cooling-controller-T21/mode"] = 1;
+    }
+);
