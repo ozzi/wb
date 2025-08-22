173bd1edfad02f83a921e036b9802b8533e8fa21
azhukov <alexander.zhukov@magenta-technology.com>
2025-08-22 16:47:59 +0400

    asic-sensor: add total asics power virtual device

---
+++ rules/asic-sensor.js
@@ -0,0 +1,35 @@
+function makeASICSensor(
+    name,
+    firstPowerTopicName,
+    secondPowerTopicName
+) {
+    var deviceName = "ASIC-sensor-" + name;
+    defineVirtualDevice(deviceName, {
+        title: "ASIC Sensor - " + name,
+        cells: {
+            power: {
+                type: "value",
+                unit: "W",
+                readonly: true,
+                value: 0
+            }
+        }
+    });
+    var powerTopicName = deviceName + "/power";
+
+    defineRule("asic-power-sensor-" + name, {
+        whenChanged: [
+            firstPowerTopicName,
+            secondPowerTopicName
+        ],
+        then: function () {
+            dev[powerTopicName] = dev[firstPowerTopicName] + dev[secondPowerTopicName];
+        }
+    });
+}
+
+makeASICSensor(
+  "TOTAL",
+  "ANTMINER T21-1/power",
+  "ANTMINER T21-2/power"
+);
